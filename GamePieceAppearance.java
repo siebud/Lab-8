70ce316f3225c5119bdf4651962c5975fcede49c
Sierra Budinoff <sierra.j.budinoff-1@ou.edu>
2019-03-23 13:43:29 -0500

    piece appearance class

---
+++ GamePieceAppearance.java
@@ -0,0 +1,19 @@
+
+public class GamePieceAppearance {
+	private Color color;
+	private Shape shape;
+	
+	public GamePieceAppearance(Color color, Shape shape) {
+		this.color = color;
+		this.shape = shape;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	public Shape getShape() {
+		return shape;
+	}
+
+}
