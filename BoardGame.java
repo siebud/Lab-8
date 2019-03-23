7dfbee16f1b8396842b94bfcab3d504279587ced
Sierra Budinoff <sierra.j.budinoff-1@ou.edu>
2019-03-23 16:05:33 -0500

    first try board game some methods complete

--- BoardGame.java
+++ BoardGame.java
@@ -1,55 +1,73 @@
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.Set;
 
 public class BoardGame {
-	protected LinkedHashMap<String, GamePiece> playerPeices;
+	protected LinkedHashMap<String, GamePiece> playerPieces;
 	protected LinkedHashMap<String, Location> playerLocations;
 	
 	public BoardGame() {
-		
+		playerPieces = new LinkedHashMap<String, GamePiece>();
+		playerLocations = new LinkedHashMap<String, Location>();
 	}
 	
 	public boolean addPlayer(String playerName, GamePiece gamePiece, Location initialLocation) {
-		
+		String plname = playerName;
+		playerPieces.put(plname, gamePiece);
+		playerLocations.put(plname, initialLocation);
+		return !playerPieces.containsValue(plname);
 	}
 	
 	public GamePiece getPlayerGamePiece(String playerName) {
-		
+		return playerPieces.get(playerName);
 	}
 	
 	public String getPlayerWithGamePiece(GamePiece gamePiece) {
-		
+		return playerPieces.get(gamePiece).name();
 	}
 	
 	public void movePlayer(String playerName, Location newLocation) {
-		
+		playerLocations.remove(playerName);
+		playerLocations.put(playerName,  newLocation);
 	}
 	
 	public String[] moveTwoPlayers(String[] playerNames, Location[] newLocations) {
+		String[] ans = new String[2];
 		
+//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+		
+		return ans;
 	}
 	
 	public Location getPlayersLocation(String player) {
-		
+		return playerLocations.get(player);
 	}
 	
 	public ArrayList<String> getPlayersAtLocation(Location loc){
-		
+		ArrayList<String> ans = new ArrayList<String>();
+//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+		System.out.println(playerLocations.get(loc));
+		return ans;
 	}
 	
 	public ArrayList<GamePiece> getGamePiecesAtLocation(Location loc){
-		
+		ArrayList<GamePiece> ans = new ArrayList<GamePiece>();
+//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA	
+		return ans;
 	}
 	
 	public Set<String> getPlayers(){
-		
+	return playerPieces.keySet();	
 	}
 	
 	public Set<Location> getPlayerLocations() {
-		
+		return (Set<Location>)playerLocations.values();
 	}
 	
 	public Set<GamePiece> getPlayerPieces(){
-		
+		return (Set<GamePiece>)playerPieces.values();
 	}
 
 }
