e4fa88d85039058d5d035a751189f6d0592993ad
Sierra Budinoff <sierra.j.budinoff-1@ou.edu>
2019-03-23 14:11:40 -0500

    game piece first try

---
+++ GamePiece.java
@@ -0,0 +1,41 @@
+
+public enum GamePiece {
+	RED_RACER(new GamePieceAppearance(Color.RED, Shape.RACECAR), 0),
+	BLUE_RACER(new GamePieceAppearance(Color.BLUE, Shape.RACECAR), 2),
+	MAGENTA_RACER(new GamePieceAppearance(Color.MAGENTA, Shape.RACECAR), 1),
+	RED_THIMBLE(new GamePieceAppearance(Color.RED, Shape.THIMBLE), 10),
+	BLUE_BOOT(new GamePieceAppearance(Color.BLUE, Shape.BOOT), 5),
+	GREEN_BOOT(new GamePieceAppearance(Color.GREEN, Shape.BOOT), 8),
+	YELLLOW_BOOT(new GamePieceAppearance(Color.YELLOW, Shape.BOOT), 7);
+	
+	private GamePieceAppearance appearance;
+	private int priority;
+	
+	private GamePiece(GamePieceAppearance appearance, int priority) {
+		this.appearance = appearance;
+		this.priority = priority;
+	}
+	
+	public Color getColor() {
+		return appearance.getColor();
+	}
+	
+	public Shape getShape() {
+		return appearance.getShape();
+	}
+	
+	public GamePiece movesFirst(GamePiece a, GamePiece b) {
+		if(a.priority < b.priority) {
+			return a;
+		}
+		else {
+			return b;
+		}
+	}
+	
+	public String toString() {
+		return String.format( "%s: a %s %s with priority %d", 
+				name(), getColor(), getShape(), priority);
+	}
+
+}
