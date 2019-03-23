9c988159cfd95e7af56f2da52ad9e02fae935c09
Sierra Budinoff <sierra.j.budinoff-1@ou.edu>
2019-03-23 12:57:57 -0500

    Color skeleton

---
+++ Color.java
@@ -0,0 +1,22 @@
+
+public enum Color {
+	RED, GREEN, BLUE, YELLOW, CYAN, MAGENTA;
+	
+	private int b;
+	private int g;
+	private int r;
+	
+
+	public int getR() {
+		return r;
+	}
+	
+	public int getG() {
+		return g;
+	}
+	
+	public int getB() {
+		return b;
+	}
+	
+}
